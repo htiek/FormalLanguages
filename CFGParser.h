dcc38d636467e363058df208fea709587651b473
Keith Schwarz <htiek@cs.stanford.edu>
2020-08-07 11:40:45 -0700

    Rename parseCFG -> CFG::parse

--- CFGParser.h
+++ CFGParser.h
@@ -4,8 +4,10 @@
 #include "CFG.h"
 #include "Utilities/JSON.h"
 
-/* Parses a CFG that was read in via the standard scanner interface. */
-CFG::CFG parseCFG(std::deque<CFG::Token> tokens, const Languages::Alphabet& alphabet);
+namespace CFG {
+    /* Parses a CFG that was read in via the standard scanner interface. */
+    CFG parse(std::deque<Token> tokens, const Languages::Alphabet& alphabet);
 
-/* Parses a CFG stored in the old style. */
-CFG::CFG parseCFG(JSON json, const Languages::Alphabet& alphabet);
+    /* Parses a CFG stored in the old style. */
+    CFG parse(JSON json, const Languages::Alphabet& alphabet);
+}
